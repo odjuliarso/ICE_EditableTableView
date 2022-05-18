f50008d46eb155d6c35f852465dcc954a98e6760
othman <othmandjuliarso@gmail.com>
2022-05-17 17:50:06 -0700

    added tableViewController on mainstoryboard

--- ICE_EditableTableView/controller/TextbookViewController.swift
+++ ICE_EditableTableView/controller/TextbookViewController.swift
@@ -10,19 +10,22 @@ import UIKit
 
 
 // This class will provide all three pieces of functionality that we need ( View controller, Data source, Event handling delegate)
-class TextbookViewController: UITableView,
-                              UITableViewDataSource,
-                              UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
+class TextbookViewController: UITableViewController {
     
     // Giving the VC access to the store
-    var bookStore : BookStore!
+    var bookStore: BookStore!
+    
+    // The method to produce the number of rows
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bookStore.allBooks.count
+    }
+            
+    // method to produce the cell for the table
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
     
     
     
--- ICE_EditableTableView/model/BookStore.swift
+++ ICE_EditableTableView/model/BookStore.swift
@@ -9,6 +9,7 @@ import Foundation
 import UIKit
 
 class BookStore {
+    
     var allBooks = [Book]()
     
     // Adding an item creation method
